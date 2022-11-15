7a57298b1389919e8e2c12ab342afd9128831b7e
Riddick <danijel.galic@live.com>
2022-11-15 08:21:35 +0100

    Created empty stub landing files for phar (#5)
    Landing file if PHAR archive is accessed
    through a web browser or console.

---
+++ src/public/index.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Main Core web public landing file
+ *
+ * This is a landing file if PHAR archive is accessed
+ * through a web browser.
+ * @since 0.1.1.pre-alpha.M1
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2023 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package FireHub\Public
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
