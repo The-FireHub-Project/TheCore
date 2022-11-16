53b9adbe05b91cab7dbcdb5bd30e9703587a3dc6
Riddick <danijel.galic@live.com>
2022-11-16 09:59:10 +0100

    Created enums for file suffix and prefix (#5)

---
+++ src/initializers/enums/firehub.Prefix.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of FireHub Web Application Framework package.
+ * @since 0.1.1.pre-alpha.M1
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2023 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package FireHub\Public
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+namespace FireHub\Initializers\Enums;
+
+/**
+ * ### Enum for available prefixes for files
+ * @since 0.1.1.pre-alpha.M1
+ */
+enum Prefix:string {
+
+    /**
+     * ### Default FireHub prefix
+     *
+     * This prefix is used by most of intenal objects.
+     * @since 0.1.1.pre-alpha.M1
+     */
+    case FIREHUB = 'firehub';
+
+}
