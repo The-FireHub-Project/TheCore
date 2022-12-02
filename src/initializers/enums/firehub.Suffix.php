9d6ab16ef58d5f427350d38e1aa8d27457dc3b8b
Riddick <danijel.galic@live.com>
2022-12-02 13:09:08 +0100

    Added Abstract case to Suffix enum

--- src/initializers/enums/firehub.Suffix.php
+++ src/initializers/enums/firehub.Suffix.php
@@ -18,7 +18,14 @@ namespace FireHub\TheCore\Initializers\Enums;
 /**
  * ### Enum for available suffixes for files
  * @since 0.1.1.pre-alpha.M1
+ * @since 0.1.3.pre-alpha.M1 Added abstract case.
  */
 enum Suffix:string {
+
+    /**
+     * ### Abstract classes
+     * @since 0.1.3.pre-alpha.M1
+     */
+    case ABSTRACT = 'abstract';
 
 }
