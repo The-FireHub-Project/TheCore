3d615518c3d4b3e96311a7888213af378ef0fea8
Riddick <danijel.galic@live.com>
2022-12-16 14:27:36 +0100

    Created Country and Continent enums (#10)

---
+++ src/support/enums/geo/firehub.Country.php
@@ -0,0 +1,308 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of FireHub Web Application Framework package
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2023 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package FireHub\Support
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+namespace FireHub\TheCore\Support\Enums\Geo;
+
+/**
+ * ### Time zones enum
+ * @since 0.2.1.pre-alpha.M2
+ *
+ * @api
+ */
+enum Country:string {
+
+    case AFGHANISTAN = 'AF';
+    case ALAND_ISLANDS = 'AX';
+    case ALBANIA = 'AL';
+    case ALGERIA = 'DZ';
+    case AMERICAN_SAMOA = 'AS';
+    case ANDORRA = 'AD';
+    case ANGOLA = 'AO';
+    case ANGUILLA = 'AI';
+    case ANTARCTICA = 'AQ';
+    case ANTIGUA_AND_BARBUDA = 'AG';
+    case ARGENTINA = 'AR';
+    case ARMENIA = 'AM';
+    case ARUBA = 'AW';
+    case AUSTRALIA = 'AU';
+    case AUSTRIA = 'AT';
+    case AZERBAIJAN = 'AZ';
+    case BAHAMAS = 'BS';
+    case BAHRAIN = 'BH';
+    case BANGLADESH = 'BD';
+    case BARBADOS = 'BB';
+    case BELARUS = 'BY';
+    case BELGIUM = 'BE';
+    case BELIZE = 'BZ';
+    case BENIN = 'BJ';
+    case BERMUDA = 'BM';
+    case BHUTAN = 'BT';
+    case BOLIVIA = 'BO';
+    case BOSNIA_AND_HERZEGOVINA = 'BA';
+    case BOTSWANA = 'BW';
+    case BOUVET_ISLAND = 'BV';
+    case BRAZIL = 'BR';
+    case BRITISH_ANTARCTIC_TERRITORY = 'BQ';
+    case BRITISH_INDIAN_OCEAN_TERRITORY = 'IO';
+    case BRITISH_VIRGIN_ISLANDS = 'VG';
+    case BRUNEI = 'BN';
+    case BULGARIA = 'BG';
+    case BURKINA_FASO = 'BF';
+    case BURUNDI = 'BI';
+    case CAMBODIA = 'KH';
+    case CAMEROON = 'CM';
+    case CANADA = 'CA';
+    case CANTON_AND_ENDERBURY_ISLANDS = 'CT';
+    case CAPE_VERDE = 'CV';
+    case CAYMAN_ISLANDS = 'KY';
+    case CENTRAL_AFRICAN_REPUBLIC = 'CF';
+    case CHAD = 'TD';
+    case CHILE = 'CL';
+    case CHINA = 'CN';
+    case CHRISTMAS_ISLAND = 'CX';
+    case COCOS_KEELING_ISLANDS = 'CC';
+    case COLOMBIA = 'CO';
+    case COMOROS = 'KM';
+    case CONGO_BRAZZAVILLE = 'CG';
+    case CONGO_KINSHASA = 'CD';
+    case COOK_ISLANDS = 'CK';
+    case COSTA_RICA = 'CR';
+    case CROATIA = 'HR';
+    case CUBA = 'CU';
+    case CYPRUS = 'CY';
+    case CZECH_REPUBLIC = 'CZ';
+    case COTE_DIVOIRE = 'CI';
+    case DENMARK = 'DK';
+    case DJIBOUTI = 'DJ';
+    case DOMINICA = 'DM';
+    case DOMINICAN_REPUBLIC = 'DO';
+    case DRONNING_MAUD_LAND = 'NQ';
+    case EAST_GERMANY = 'DD';
+    case ECUADOR = 'EC';
+    case EGYPT = 'EG';
+    case EL_SALVADOR = 'SV';
+    case EQUATORIAL_GUINEA = 'GQ';
+    case ERITREA = 'ER';
+    case ESTONIA = 'EE';
+    case ETHIOPIA = 'ET';
+    case FALKLAND_ISLANDS = 'FK';
+    case FAROE_ISLANDS = 'FO';
+    case FIJI = 'FJ';
+    case FINLAND = 'FI';
+    case FRANCE = 'FR';
+    case FRENCH_GUIANA = 'GF';
+    case FRENCH_POLYNESIA = 'PF';
+    case FRENCH_SOUTHERN_TERRITORIES = 'TF';
+    case FRENCH_SOUTHERN_AND_ANTARCTIC_TERRITORIES = 'FQ';
+    case GABON = 'GA';
+    case GAMBIA = 'GM';
+    case GEORGIA = 'GE';
+    case GERMANY = 'DE';
+    case GHANA = 'GH';
+    case GIBRALTAR = 'GI';
+    case GREECE = 'GR';
+    case GREENLAND = 'GL';
+    case GRENADA = 'GD';
+    case GUADELOUPE = 'GP';
+    case GUAM = 'GU';
+    case GUATEMALA = 'GT';
+    case GUERNSEY = 'GG';
+    case GUINEA = 'GN';
+    case GUINEA_BISSAU = 'GW';
+    case GUYANA = 'GY';
+    case HAITI = 'HT';
+    case HEARD_ISLAND_AND_MCDONALD_ISLANDS = 'HM';
+    case HONDURAS = 'HN';
+    case HONG_KONG_SAR_CHINA = 'HK';
+    case HUNGARY = 'HU';
+    case ICELAND = 'IS';
+    case INDIA = 'IN';
+    case INDONESIA = 'ID';
+    case IRAN = 'IR';
+    case IRAQ = 'IQ';
+    case IRELAND = 'IE';
+    case ISLE_OF_MAN = 'IM';
+    case ISRAEL = 'IL';
+    case ITALY = 'IT';
+    case JAMAICA = 'JM';
+    case JAPAN = 'JP';
+    case JERSEY = 'JE';
+    case JOHNSTON_ISLAND = 'JT';
+    case JORDAN = 'JO';
+    case KAZAKHSTAN = 'KZ';
+    case KENYA = 'KE';
+    case KIRIBATI = 'KI';
+    case KUWAIT = 'KW';
+    case KYRGYZSTAN = 'KG';
+    case LAOS = 'LA';
+    case LATVIA = 'LV';
+    case LEBANON = 'LB';
+    case LESOTHO = 'LS';
+    case LIBERIA = 'LR';
+    case LIBYA = 'LY';
+    case LIECHTENSTEIN = 'LI';
+    case LITHUANIA = 'LT';
+    case LUXEMBOURG = 'LU';
+    case MACAU_SAR_CHINA = 'MO';
+    case MACEDONIA = 'MK';
+    case MADAGASCAR = 'MG';
+    case MALAWI = 'MW';
+    case MALAYSIA = 'MY';
+    case MALDIVES = 'MV';
+    case MALI = 'ML';
+    case MALTA = 'MT';
+    case MARSHALL_ISLANDS = 'MH';
+    case MARTINIQUE = 'MQ';
+    case MAURITANIA = 'MR';
+    case MAURITIUS = 'MU';
+    case MAYOTTE = 'YT';
+    case METROPOLITAN_FRANCE = 'FX';
+    case MEXICO = 'MX';
+    case MICRONESIA = 'FM';
+    case MIDWAY_ISLANDS = 'MI';
+    case MOLDOVA = 'MD';
+    case MONACO = 'MC';
+    case MONGOLIA = 'MN';
+    case MONTENEGRO = 'ME';
+    case MONTSERRAT = 'MS';
+    case MOROCCO = 'MA';
+    case MOZAMBIQUE = 'MZ';
+    case MYANMAR_BURMA = 'MM';
+    case NAMIBIA = 'NA';
+    case NAURU = 'NR';
+    case NEPAL = 'NP';
+    case NETHERLANDS = 'NL';
+    case NETHERLANDS_ANTILLES = 'AN';
+    case NEW_CALEDONIA = 'NC';
+    case NEW_ZEALAND = 'NZ';
+    case NICARAGUA = 'NI';
+    case NIGER = 'NE';
+    case NIGERIA = 'NG';
+    case NIUE = 'NU';
+    case NORFOLK_ISLAND = 'NF';
+    case NORTH_KOREA = 'KP';
+    case NORTH_VIETNAM = 'VD';
+    case NORTHERN_MARIANA_ISLANDS = 'MP';
+    case NORWAY = 'NO';
+    case OMAN = 'OM';
+    case PACIFIC_ISLANDS_TRUST_TERRITORY = 'PC';
+    case PAKISTAN = 'PK';
+    case PALAU = 'PW';
+    case PALESTINIAN_TERRITORIES = 'PS';
+    case PANAMA = 'PA';
+    case PANAMA_CANAL_ZONE = 'PZ';
+    case PAPUA_NEW_GUINEA = 'PG';
+    case PARAGUAY = 'PY';
+    case PEOPLES_DEMOCRATIC_REPUBLIC_OF_YEMEN = 'YD';
+    case PERU = 'PE';
+    case PHILIPPINES = 'PH';
+    case PITCAIRN_ISLANDS = 'PN';
+    case POLAND = 'PL';
+    case PORTUGAL = 'PT';
+    case PUERTO_RICO = 'PR';
+    case QATAR = 'QA';
+    case ROMANIA = 'RO';
+    case RUSSIA = 'RU';
+    case RWANDA = 'RW';
+    case REUNION = 'RE';
+    case SAINT_BARTHELEMY = 'BL';
+    case SAINT_HELENA = 'SH';
+    case SAINT_KITTS_AND_NEVIS = 'KN';
+    case SAINT_LUCIA = 'LC';
+    case SAINT_MARTIN = 'MF';
+    case SAINT_PIERRE_AND_MIQUELON = 'PM';
+    case SAINT_VINCENT_AND_THE_GRENADINES = 'VC';
+    case SAMOA = 'WS';
+    case SAN_MARINO = 'SM';
+    case SAUDI_ARABIA = 'SA';
+    case SENEGAL = 'SN';
+    case SERBIA = 'RS';
+    case SERBIA_AND_MONTENEGRO = 'CS';
+    case SEYCHELLES = 'SC';
+    case SIERRA_LEONE = 'SL';
+    case SINGAPORE = 'SG';
+    case SLOVAKIA = 'SK';
+    case SLOVENIA = 'SI';
+    case SOLOMON_ISLANDS = 'SB';
+    case SOMALIA = 'SO';
+    case SOUTH_AFRICA = 'ZA';
+    case SOUTH_GEORGIA_AND_THE_SOUTH_SANDWICH_ISLANDS = 'GS';
+    case SOUTH_KOREA = 'KR';
+    case SPAIN = 'ES';
+    case SRI_LANKA = 'LK';
+    case SUDAN = 'SD';
+    case SURINAME = 'SR';
+    case SVALBARD_AND_JAN_MAYEN = 'SJ';
+    case SWAZILAND = 'SZ';
+    case SWEDEN = 'SE';
+    case SWITZERLAND = 'CH';
+    case SYRIA = 'SY';
+    case SAO_TOME_AND_PRINCIPE = 'ST';
+    case TAIWAN = 'TW';
+    case TAJIKISTAN = 'TJ';
+    case TANZANIA = 'TZ';
+    case THAILAND = 'TH';
+    case TIMOR_LESTE = 'TL';
+    case TOGO = 'TG';
+    case TOKELAU = 'TK';
+    case TONGA = 'TO';
+    case TRINIDAD_AND_TOBAGO = 'TT';
+    case TUNISIA = 'TN';
+    case TURKEY = 'TR';
+    case TURKMENISTAN = 'TM';
+    case TURKS_AND_CAICOS_ISLANDS = 'TC';
+    case TUVALU = 'TV';
+    case US_MINOR_OUTLYING_ISLANDS = 'UM';
+    case US_MISCELLANEOUS_PACIFIC_ISLANDS = 'PU';
+    case US_VIRGIN_ISLANDS = 'VI';
+    case UGANDA = 'UG';
+    case UKRAINE = 'UA';
+    case UNION_OF_SOVIET_SOCIALIST_REPUBLICS = 'SU';
+    case UNITED_ARAB_EMIRATES = 'AE';
+    case UNITED_KINGDOM = 'GB';
+    case UNITED_STATES = 'US';
+    case URUGUAY = 'UY';
+    case UZBEKISTAN = 'UZ';
+    case VANUATU = 'VU';
+    case VATICAN_CITY = 'VA';
+    case VENEZUELA = 'VE';
+    case VIETNAM = 'VN';
+    case WAKE_ISLAND = 'WK';
+    case WALLIS_AND_FUTUNA = 'WF';
+    case WESTERN_SAHARA = 'EH';
+    case YEMEN = 'YE';
+    case ZAMBIA = 'ZM';
+    case ZIMBABWE = 'ZW';
+
+    /**
+    * ### Gets continent for country
+    * @since 0.2.1.pre-alpha.M2
+    *
+    * @return \FireHub\TheCore\Support\Enums\Geo\Continent Continent enum of selected country.
+    */
+    public function continent ():Continent {
+
+        return match ($this) {
+            self::ALGERIA, self::ANGOLA, self::BENIN, self::BOTSWANA, self::BURKINA_FASO, self::BURUNDI, self::CAMEROON, self::CAPE_VERDE, self::CENTRAL_AFRICAN_REPUBLIC, self::CHAD, self::CONGO_KINSHASA, self::COMOROS, self::CONGO_BRAZZAVILLE, self::COTE_DIVOIRE, self::DJIBOUTI, self::EGYPT, self::ERITREA, self::EQUATORIAL_GUINEA, self::ETHIOPIA, self::GABON, self::GAMBIA, self::GHANA, self::GUINEA, self::GUINEA_BISSAU, self::KENYA, self::LESOTHO, self::LIBERIA, self::LIBYA, self::MADAGASCAR, self::MALAWI, self::MALI, self::MAURITANIA, self::MAURITIUS, self::MAYOTTE, self::MOROCCO, self::MOZAMBIQUE, self::NAMIBIA, self::NIGER, self::NIGERIA, self::RWANDA, self::REUNION, self::SAINT_HELENA, self::SENEGAL, self::SEYCHELLES, self::SIERRA_LEONE, self::SOMALIA, self::SOUTH_AFRICA, self::SUDAN, self::SWAZILAND, self::SAO_TOME_AND_PRINCIPE, self::TANZANIA, self::TOGO, self::TONGA, self::TUNISIA, self::UGANDA, self::ZIMBABWE, self::ZAMBIA, self::WESTERN_SAHARA => Continent::AFRICA,
+            self::ANTARCTICA, self::BOUVET_ISLAND, self::BRITISH_ANTARCTIC_TERRITORY, self::DRONNING_MAUD_LAND, self::FRENCH_SOUTHERN_TERRITORIES, self::FRENCH_SOUTHERN_AND_ANTARCTIC_TERRITORIES => Continent::ANTARCTICA,
+            self::AFGHANISTAN, self::ARMENIA, self::AZERBAIJAN, self::BAHRAIN, self::BANGLADESH, self::BHUTAN, self::BRITISH_INDIAN_OCEAN_TERRITORY, self::BRUNEI, self::CAMBODIA, self::CHINA, self::GEORGIA, self::HONG_KONG_SAR_CHINA, self::INDIA, self::INDONESIA, self::IRAN, self::IRAQ, self::ISRAEL, self::JAPAN, self::JORDAN, self::KAZAKHSTAN, self::KUWAIT, self::KYRGYZSTAN, self::LAOS, self::LEBANON, self::MACAU_SAR_CHINA, self::MALAYSIA, self::MALDIVES, self::MONGOLIA, self::MYANMAR_BURMA, self::NEPAL, self::NORTH_KOREA, self::NORTH_VIETNAM, self::OMAN, self::PAKISTAN, self::PALESTINIAN_TERRITORIES, self::PEOPLES_DEMOCRATIC_REPUBLIC_OF_YEMEN, self::PHILIPPINES, self::QATAR, self::SAUDI_ARABIA, self::SINGAPORE, self::SOUTH_KOREA, self::SRI_LANKA, self::SYRIA, self::TAIWAN, self::TAJIKISTAN, self::THAILAND, self::TIMOR_LESTE, self::TURKEY, self::TURKMENISTAN, self::UNITED_ARAB_EMIRATES, self::YEMEN, self::UZBEKISTAN, self::VIETNAM => Continent::ASIA,
+            self::ALAND_ISLANDS, self::ALBANIA, self::ANDORRA, self::AUSTRIA, self::BELARUS, self::BELGIUM, self::BOSNIA_AND_HERZEGOVINA, self::BULGARIA, self::CROATIA, self::CYPRUS, self::CZECH_REPUBLIC, self::DENMARK, self::EAST_GERMANY, self::ESTONIA, self::FAROE_ISLANDS, self::FINLAND, self::FRANCE, self::GERMANY, self::GIBRALTAR, self::GREECE, self::GUERNSEY, self::HUNGARY, self::ICELAND, self::IRELAND, self::ISLE_OF_MAN, self::ITALY, self::JERSEY, self::LATVIA, self::LIECHTENSTEIN, self::LITHUANIA, self::LUXEMBOURG, self::MACEDONIA, self::MALTA, self::METROPOLITAN_FRANCE, self::MOLDOVA, self::MONACO, self::MONTENEGRO, self::NETHERLANDS, self::NORWAY, self::POLAND, self::PORTUGAL, self::ROMANIA, self::RUSSIA, self::SAN_MARINO, self::SERBIA, self::SERBIA_AND_MONTENEGRO, self::SLOVAKIA, self::SLOVENIA, self::SPAIN, self::SVALBARD_AND_JAN_MAYEN, self::SWEDEN, self::SWITZERLAND, self::UKRAINE, self::UNION_OF_SOVIET_SOCIALIST_REPUBLICS, self::UNITED_KINGDOM, self::VATICAN_CITY => Continent::EUROPE,
+            self::ANGUILLA, self::ANTIGUA_AND_BARBUDA, self::ARUBA, self::BAHAMAS, self::BARBADOS, self::BELIZE, self::BERMUDA, self::BRITISH_VIRGIN_ISLANDS, self::CANADA, self::CAYMAN_ISLANDS, self::COSTA_RICA, self::CUBA, self::DOMINICA, self::DOMINICAN_REPUBLIC, self::EL_SALVADOR, self::GREENLAND, self::GRENADA, self::GUADELOUPE, self::GUATEMALA, self::HAITI, self::HONDURAS, self::JAMAICA, self::JOHNSTON_ISLAND, self::MARTINIQUE, self::MEXICO, self::MONTSERRAT, self::NICARAGUA, self::PANAMA, self::PANAMA_CANAL_ZONE, self::PUERTO_RICO, self::SAINT_BARTHELEMY, self::SAINT_KITTS_AND_NEVIS, self::SAINT_LUCIA, self::SAINT_MARTIN, self::SAINT_PIERRE_AND_MIQUELON, self::SAINT_VINCENT_AND_THE_GRENADINES, self::TRINIDAD_AND_TOBAGO, self::TURKS_AND_CAICOS_ISLANDS, self::US_MINOR_OUTLYING_ISLANDS, self::US_MISCELLANEOUS_PACIFIC_ISLANDS, self::US_VIRGIN_ISLANDS, self::UNITED_STATES => Continent::NORTH_AMERICA,
+            self::AMERICAN_SAMOA, self::AUSTRALIA, self::CANTON_AND_ENDERBURY_ISLANDS, self::CHRISTMAS_ISLAND, self::COCOS_KEELING_ISLANDS, self::COOK_ISLANDS, self::FIJI, self::FRENCH_POLYNESIA, self::GUAM, self::HEARD_ISLAND_AND_MCDONALD_ISLANDS, self::KIRIBATI, self::MARSHALL_ISLANDS, self::MICRONESIA, self::MIDWAY_ISLANDS, self::NAURU, self::NEW_CALEDONIA, self::NEW_ZEALAND, self::NIUE, self::NORFOLK_ISLAND, self::NORTHERN_MARIANA_ISLANDS, self::PACIFIC_ISLANDS_TRUST_TERRITORY, self::PALAU, self::PAPUA_NEW_GUINEA, self::PITCAIRN_ISLANDS, self::SAMOA, self::SOLOMON_ISLANDS, self::TOKELAU, self::TUVALU, self::WAKE_ISLAND, self::VANUATU, self::WALLIS_AND_FUTUNA => Continent::OCEANIA,
+            self::ARGENTINA, self::BOLIVIA, self::BRAZIL, self::CHILE, self::COLOMBIA, self::ECUADOR, self::FALKLAND_ISLANDS, self::FRENCH_GUIANA, self::GUYANA, self::NETHERLANDS_ANTILLES, self::PARAGUAY, self::PERU, self::SOUTH_GEORGIA_AND_THE_SOUTH_SANDWICH_ISLANDS, self::SURINAME, self::URUGUAY, self::VENEZUELA => Continent::SOUTH_AMERICA
+        };
+
+    }
+
+}
